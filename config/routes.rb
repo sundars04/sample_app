fffb6fc701fe8682b28095b86d0ea08835164366
Sundars0 <sundars04@outlook.com>
2018-03-31 15:05:24 +0000

    Finish user signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
 
-  get 'users/new'
 
   root 'static_pages#home'
 
@@ -8,6 +7,9 @@ Rails.application.routes.draw do
   get '/about',   to: 'static_pages#about'
   get '/contact', to: 'static_pages#contact'
   get '/signup',  to: 'users#new'
+  post '/signup',  to: 'users#create'
+  # get 'users/new'
+  resources :users
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
